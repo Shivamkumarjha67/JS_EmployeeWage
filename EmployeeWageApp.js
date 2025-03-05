65e086dde7cf48cafd2eb23698799b07738d3c46
Shivam Kumar Jha <shkrjh@gmail.com>
2025-03-05 15:02:17 +0530

    [Shivam Kumar Jha] Add . Calculating the employee wages based on full time or part time

--- EmployeeWageApp.js
+++ EmployeeWageApp.js
@@ -7,3 +7,19 @@ if (empCheck == IS_ABSENT) {
     console.log("Employee is Present");
 }
 
+// UC2: Calculate Daily Employee Wage based on Part-time or Full-time
+const PART_TIME = 1;
+const FULL_TIME = 2;
+const WAGE_PER_HOUR = 20;
+
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case PART_TIME: return 4;
+        case FULL_TIME: return 8;
+        default: return 0;
+    }
+}
+
+let empHrs = getWorkingHours(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("UC2 - Emp Wage: " + empWage);
